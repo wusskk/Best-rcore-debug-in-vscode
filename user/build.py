19347db5878d85e2bf74ff8eaa41d63a953b03f9
wusskk <shikaicang@163.com>
2024-04-22 11:39:35 +0800

    update 2

---
+++ user/build.py
@@ -0,0 +1,28 @@
+import os
+
+base_address = 0x80400000
+step = 0x20000
+linker = "src/linker.ld"
+
+app_id = 0
+apps = os.listdir("src/bin/")
+apps.sort()
+chapter = os.getenv("CHAPTER")
+mode = os.getenv("MODE", default = "debug")
+if mode == "release" :
+	mode_arg = "--release"
+else :
+    mode_arg = ""
+
+for app in apps:
+    app = app[: app.find(".")]
+    os.system(
+        "cargo rustc -v --bin %s %s -- -Clink-args=-Ttext=%x"
+        % (app, mode_arg, base_address + step * app_id)
+    )
+    print(
+        "[build.py] application %s start with address %s"
+        % (app, hex(base_address + step * app_id))
+    )
+    if chapter == '3':
+        app_id = app_id + 1
